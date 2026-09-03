c0831b11a73f1b37a81516289f6de45d6a386da0
Shubham Mukhopadhyay <117881946+ShubhamMukhopadhyay@users.noreply.github.com>
2026-09-03 10:50:11 +0530

    Solved: Validate Binary Search Tree [Medium]

---
+++ LeetCode/Medium/Validate Binary Search Tree/solution.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def check(self, root, mn, mx):
+        if root is None:
+            return True
+
+        if root.val < mn or root.val > mx:
+            return False
+
+        checkLeft = self.check(root.left, mn, root.val - 1 )
+        checkRight = self.check(root.right, root.val + 1, mx )
+
+        return checkLeft and checkRight
+
+    def isValidBST(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        return self.check(root, -100000000000000, 100000000000000)
