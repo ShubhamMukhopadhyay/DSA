e55e43f1064428104bff84388a34cd9183c0bedb
Shubham Mukhopadhyay <117881946+ShubhamMukhopadhyay@users.noreply.github.com>
2026-09-08 13:54:24 +0530

    Solved: Find the Index of the First Occurrence in a String [Easy]

---
+++ LeetCode/Easy/Find the Index of the First Occurrence in a String/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        h = len(haystack)
+        n = len(needle)
+
+        for i in range(h - n + 1):
+            window = haystack[i:i+n]
+
+            if window == needle:
+                return i
+
+        return -1
+        
