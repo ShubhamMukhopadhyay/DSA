3e84046a428f7e3ae13edffe905e4ade6dbde55c
Shubham Mukhopadhyay <117881946+ShubhamMukhopadhyay@users.noreply.github.com>
2026-09-21 14:46:19 +0530

    Solved: Rearrange Array Elements by Sign [Medium]

---
+++ Two Pointers/Medium/Rearrange Array Elements by Sign/solution.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def rearrangeArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        pos, neg = [], []
+
+        for num in nums:
+            if num > 0:
+                pos.append(num)
+
+            else:
+                neg.append(num)
+
+        i = 0
+
+        while (2 * i) < len(nums):
+            nums[2 * i] = pos[i]
+            nums[2 * i + 1] = neg[i]
+
+            i += 1
+
+        return nums
